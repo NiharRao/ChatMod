a75556d204cb342a981ba6f4d843f1f2dea252b4
Nihar <nihar@uoguelph.ca>
2019-06-11 15:22:51 -0400

    purge <amount> works, will implement user specific purging

--- bot.js
+++ bot.js
@@ -2,15 +2,41 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('Logged in as ${client.user.tag}!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-	//console.log('msg sent');
-  if (msg.content === '!ping') {
-    //msg.reply('Pong!');
-	msg.delete();
-	console.log('msg deleted');
+client.on('message', message => {
+  // console.log('msg sent');
+  var prefix ='!';
+  var msg = message.content.toLowerCase();
+  var sender = message.author;
+  var command = message.content.slice(prefix.length).split(" ");
+  var args = command.slice(1);
+
+  if (msg.startsWith(prefix+"purge")) {
+  // msg.reply('Pong!');
+    //message.delete();
+    async function purge(){
+      
+
+      if(message.author.username != "EnderRao"){
+        return;
+      }
+
+      if (isNaN(args[0])){
+        message.channel.send('Usage: ' + prefix + 'purge <amount>');
+        return;
+      }
+
+      var del = parseInt(args[0])+1;
+      const fetched = await message.channel.fetchMessages({limit: del});
+      console.log(fetched.size + ' messages are being deleted');
+
+      message.channel.bulkDelete(fetched)
+        .catch(error => message.channel.send(`Error: $(error)`));
+
+    }
+    purge();
 	
   }
 });
