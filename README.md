92415bae8704dad62da204b23419d3c2c7fc87e7
Nihar <nihar@uoguelph.ca>
2019-06-15 00:09:21 -0400

    word filter complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ChatMod
 A discord bot for simple chat moderation (delete specific number of recent messages sent by specific user)
+
+Commands:
+1. !purge N
+	- N represents an integer. This allows a user with administrator permissions to purge N recent messages in the channel the command is used in. 
+
+2. !purge N @user
+	- Same command, but this specifies to purge the messages only from the mentioned user.
--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var fs = require('fs');
+var filterList = [];
+var filter = false;
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  var fileContents = fs.readFileSync('./words.txt', 'utf8');
+  filterList = JSON.parse(fileContents);
+  var fileContents2 = fs.readFileSync('./filterSwitch.txt', 'utf8');
+  filter = JSON.parse(fileContents2);
+  //console.log(filterList);
 });
 
 client.on('message', message => {
@@ -13,13 +22,44 @@ client.on('message', message => {
   var command = message.content.slice(prefix.length).split(" ");
   var args = command.slice(1);
 
-  if(message.content.match(/test/i)){
-    message.reply('found!');
-    //message.delete();
-    return;
+  
+  if(msg.startsWith(prefix+"filter")){
+    if(!(message.member.hasPermission("ADMINISTRATOR"))){
+      message.reply('Insufficient permissions');
+      return;
+    }
+    if((args.length < 1) || (args.length > 2)){
+      message.channel.send('Usage:\n'+ prefix + 'filter show\n' + prefix + 'filter enable\n' + prefix + 'filter disable\n' + prefix + 'filter add <word>\n' + prefix + 'filter remove <word>');
+      return;
+    }
+    if(args[0].toLowerCase() === 'add'){
+      addToFilter(args[1].toLowerCase(), message);
+    }
+    else if(args[0].toLowerCase() === 'remove'){
+      removeFromFilter(args[1].toLowerCase(), message);
+    }
+    else if(args[0].toLowerCase() === 'show'){
+      showList(message);
+    }
+    else if(args[0].toLowerCase() === 'enable'){
+      filter = true;
+      message.reply("Message filter enabled");
+      var file = fs.createWriteStream('filterSwitch.txt');
+      file.write(JSON.stringify(filter));
+    }
+    else if(args[0].toLowerCase() === 'disable'){
+      filter = false;
+      message.reply("Message filter disabled");
+      var file = fs.createWriteStream('filterSwitch.txt');
+      file.write(JSON.stringify(filter));
+    }
+    else{
+      message.channel.send('Usage:\n'+ prefix + 'filter show\n' + prefix + 'filter enable\n' + prefix + 'filter disable\n' + prefix + 'filter add <word>\n' + prefix + 'filter remove <word>');
+      return;
+    }
   }
 
-  if (msg.startsWith(prefix+"purge")) {
+  else if (msg.startsWith(prefix+"purge")) {
     async function purge(){
       //check to mnake sure EnderRao is the one purging
       if(!(message.member.hasPermission("ADMINISTRATOR"))){
@@ -73,12 +113,80 @@ client.on('message', message => {
           console.log('Error: '+error);
         });
       }
+      else{
+        message.channel.send('Usage:\n' + prefix + 'purge <amount>\n' + prefix + 'purge <amount> <@user>');
+        return;
+      }
     }
     purge();
   }
 
+  else{
+    if(message.member.roles.find(r => r.name === "ChatMod")){
+      return;
+    }
+    if(filter){
+      for(var i = 0; i < filterList.length; i++){
+        var word = filterList[i];
+        var regex = new RegExp(word,"i");
+        if(message.content.match(regex)){
+          console.log('detected '+filterList[i]);
+          message.reply("Don't say that!");
+          message.delete();
+          return;
+        }
+      }
+    }
+  }
 
 });
 
+function addToFilter(word, message){
+  for(var i = 0; i < filterList.length;i++){
+    if(filterList[i] === word){
+      return;
+    }
+  }
+  filterList.push(word);
+  console.log('Added '+word+' to filter list.');
+  message.channel.send('Added new word to filter list');
+
+  var file = fs.createWriteStream('words.txt');
+  file.write(JSON.stringify(filterList));
+}
+
+function removeFromFilter(word, message){
+  for( var i = 0; i < filterList.length; i++){ 
+    if ( filterList[i] === word) {
+      filterList.splice(i, 1);
+      console.log('Removed '+word+' from filter list.');
+      message.channel.send('Removed word from filter list');
+      i--;
+    }
+  }
+  var file = fs.createWriteStream('words.txt');
+  file.write(JSON.stringify(filterList));
+}
+
+function showList(message){
+  var words = "[";
+  for(var i = 0; i < filterList.length; i++){
+    if(i == filterList.length-1){
+      words = words + filterList[i]+']';
+    }
+    else{
+      words = words + filterList[i]+", ";
+    }
+  }
+  if(filterList.length == 0){
+    words = "[]";
+  }
+  if(filter){
+    message.channel.send('Filter list (enabled):\n'+words);
+  }
+  else{
+    message.channel.send('Filter list (disabled):\n'+words);
+  }
+}
 
 client.login(process.env.BOT_TOKEN);
